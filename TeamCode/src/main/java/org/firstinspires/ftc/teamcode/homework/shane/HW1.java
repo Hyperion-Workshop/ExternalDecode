f4dffe7ed31edc12dd0b70ecf7b21759e2666996
sogsog1 <shanelucaslin@gmail.com>
2026-04-26 16:13:26 -0700

    Update HW1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/shane/HW1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/shane/HW1.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.homework.shane;
 
 public class HW1 {
+    public static void main(String[] args) {
+        System.out.print(getFibonacci(10));
+    }
+
+    public static int getFibonacci(int index) {
+        int result = 0;
+        if (index < 0) {
+            return result;
+        }
+        int first = 0;
+        int second = 1;
+        int third;
+        for (int i = 2; i < index; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
 }
