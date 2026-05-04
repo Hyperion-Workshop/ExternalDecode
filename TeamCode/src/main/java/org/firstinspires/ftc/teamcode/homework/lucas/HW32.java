e960fcbe788cb169ecee51785430acbcaadd7d19
Paroot24 <lucas.l.chen24@gmail.com>
2026-05-03 20:24:25 -0700

    Update HW32.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/lucas/HW32.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/lucas/HW32.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.homework.lucas;
 
-public class HW32 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hyperionModules.HyperionColorSensor;
+import org.firstinspires.ftc.teamcode.hyperionModules.HyperionTelemetry;
+
+@Autonomous(name = "color sensor")
+public class HW32 extends LinearOpMode{
+    HyperionColorSensor hyperionColorSensor;
+    HyperionTelemetry hyperionTelemetry;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        while (opModeInInit()) {
+            hyperionColorSensor = new HyperionColorSensor(hardwareMap, "colorSensor");
+            hyperionTelemetry = new HyperionTelemetry(telemetry);
+        }
+        while (opModeIsActive()) {
+            hyperionTelemetry.addData("RGB Values: " , hyperionColorSensor.getRGBValues());
+            hyperionTelemetry.addData("Artifact Color: " , hyperionColorSensor.getArtifactColor());
+        }
+    }
 }
