5664e2bbb4e6d4d441c0bacaf0b2f00498fe59b3
sogsog1 <shanelucaslin@gmail.com>
2026-05-17 17:16:04 -0700

    HW5a
    did hw5a (create state machine for intake motor)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/shane/HW5a.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/shane/HW5a.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode.homework.shane;
 
 
+import static org.firstinspires.ftc.teamcode.homework.shane.HW5a.State.ACTIVE;
+import static org.firstinspires.ftc.teamcode.homework.shane.HW5a.State.IDLE;
 
-import com.qualcomm.robotcore.hardware.DcMotorImpl;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.logging.ILogger;
 import org.firstinspires.ftc.teamcode.state.BaseIOStateMachine;
 
-public class HW5a extends BaseIOStateMachine {
+public class HW5a extends BaseIOStateMachine<HW5a.State> {
     DcMotorImplEx motor;
 
     public HW5a(ILogger logger) {
@@ -17,13 +18,22 @@ public class HW5a extends BaseIOStateMachine {
     }
 
     @Override
-    public void init() {
-
+    protected void init() {
+        switchState(State.IDLE);
     }
 
     @Override
-    public void tick() {
-
+    protected void tick() {
+        switch(getCurrentState()) {
+            case IDLE:
+                motor.setPower(0);
+                setStateChangeReadiness(!switchToQueued());
+                break;
+            case ACTIVE:
+                motor.setPower(1);
+                setStateChangeReadiness(!switchToQueued());
+                break;
+        }
     }
 
     public void initHw(HardwareMap hardwareMap) {
@@ -31,6 +41,6 @@ public class HW5a extends BaseIOStateMachine {
     }
 
     public enum State {
-        IDLE, STATE
+        IDLE, ACTIVE
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/homework/shane/HW5b.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.homework.shane;
+
+import org.firstinspires.ftc.teamcode.state.BaseIOStateMachine;
+
+public class HW5b extends BaseIOStateMachine {
+
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void tick() {
+
+    }
+
+    public enum State {
+        IDLE, ACTIVE
+    }
+
+}
